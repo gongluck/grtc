ec4036d3344328c823485879796e91618b4a8533
gongluck <gongluck93@gmail.com>
2025-01-31 01:31:20 +0800

    cgo ndk build

--- build.sh
+++ build.sh
@@ -1,19 +1,33 @@
-#!/bin/sh
+#!/bin/bash
 
+# 设置项目根目录
 root=$(pwd)
-# rm -r $root/bin
+rm -r $root/bin
 
+# 编译 signaling_server
 cd $root/signaling_server/src
 go build -o ../../bin/signaling_server/signaling_server main.go
 
+# 编译 website_server
 cd $root/website_server/src
 go build -o ../../bin/website_server/website_server main.go
 cp -r templates ../../bin/website_server/
 
+# 编译 pusher
 cd $root/pusher/src
-apt install gcc-arm-linux-gnueabi
-GOOS=android GOARCH=arm64 go build -tags android -o ../../bin/pusher/pusher_android_arm64 main.go
-
-export GOOS=windows
-export GOARCH=amd64
-go build -o ../../bin/pusher/pusher_windows_amd64.exe main.go
+go clean
+# 设置环境变量
+ANDROID_NDK_BIN=$ANDROID_NDK_HOME/toolchains/llvm/prebuilt/darwin-x86_64/bin
+ANDROID_NDK_SYSROOT=$ANDROID_NDK_HOME/toolchains/llvm/prebuilt/darwin-x86_64/sysroot
+export GOOS=android
+export GOARCH=arm64
+export CGO_ENABLED=1
+export CC=$ANDROID_NDK_BIN/aarch64-linux-android21-clang
+export CXX=$ANDROID_NDK_BIN/aarch64-linux-android21-clang++
+export CGO_CFLAGS=""
+export CGO_CXXFLAGS=""
+export CGO_LDFLAGS="$ANDROID_NDK_SYSROOT/usr/lib/aarch64-linux-android/21/libc++.a"
+# 生成 C++ 对象文件
+go generate -tags android ./...
+# 编译 Go 代码
+go build -tags android -o ../../bin/pusher/pusher_android_arm64 main.go
--- pusher/src/webrtc/wrapper_android.go
+++ pusher/src/webrtc/wrapper_android.go
@@ -5,23 +5,24 @@
  * @Author: gongluck
  * @Date: 2025-01-29 22:41:22
  * @Last Modified by: gongluck
- * @Last Modified time: 2025-01-30 01:24:45
+ * @Last Modified time: 2025-01-31 01:29:25
  */
 
 package webrtc
 
-//go:generate $ANDROID_NDK_HOME/toolchains/llvm/prebuilt/darwin-x86_64/bin/clang++ -target aarch64-linux-android21 -std=c++11 -c ./cpp/c++.cpp -o ./cpp/c++.o
+//go:generate rm -f ./cpp/c++_android.o
+//go:generate ${CC} -c ./cpp/c++.cpp -o ./cpp/c++_android.o
 
 /*
-#cgo CXXFLAGS: -std=c++11
 #cgo CFLAGS: -I${SRCDIR}/cpp
-#cgo LDFLAGS: ${SRCDIR}/cpp/c++.o -lstdc++ -llog -landroid
+#cgo CXXFLAGS: -I${SRCDIR}/cpp
+#cgo LDFLAGS: -L${SRCDIR}/cpp ${SRCDIR}/cpp/c++_android.o
 #include "c++.h"
 */
 import "C"
 import "log"
 
 func CallCppFunction() {
-	log.Println("Calling CppFunction()...")
+	log.Println("Calling android CppFunction()...")
 	C.CppFunction()
 }
